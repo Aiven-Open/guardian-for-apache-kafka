3f1dc7815c3f6d7749fefe6b4afd1cf1b6ce89cd
Matthew de Detrich <matthew.dedetrich@aiven.io>
2021-08-10 23:58:10 +0200

    Refactor ConfigReader definitions to use SAM

--- core-s3/src/main/scala/io/aiven/guardian/kafka/s3/Config.scala
+++ core-s3/src/main/scala/io/aiven/guardian/kafka/s3/Config.scala
@@ -13,56 +13,50 @@ trait Config {
   // for S3Headers apart from the limited S3Headers(). This means we can't use pureconfig.generic.auto._ and hence
   // we have to write this out manually
 
-  implicit val cannedACLConfigReader = new ConfigReader[CannedAcl] {
-    override def from(cur: ConfigCursor): Result[CannedAcl] =
-      cur.asString.flatMap {
-        case CannedAcl.AuthenticatedRead.value      => Right(CannedAcl.AuthenticatedRead)
-        case CannedAcl.AwsExecRead.value            => Right(CannedAcl.AwsExecRead)
-        case CannedAcl.BucketOwnerFullControl.value => Right(CannedAcl.BucketOwnerFullControl)
-        case CannedAcl.BucketOwnerRead.value        => Right(CannedAcl.BucketOwnerRead)
-        case CannedAcl.Private.value                => Right(CannedAcl.Private)
-        case CannedAcl.PublicRead.value             => Right(CannedAcl.PublicRead)
-        case CannedAcl.PublicReadWrite.value        => Right(CannedAcl.PublicReadWrite)
-        case rest                                   => Left(failure(cur, rest, "CannedAcl"))
-      }
-  }
+  implicit val cannedACLConfigReader: ConfigReader[CannedAcl] = (cur: ConfigCursor) =>
+    cur.asString.flatMap {
+      case CannedAcl.AuthenticatedRead.value      => Right(CannedAcl.AuthenticatedRead)
+      case CannedAcl.AwsExecRead.value            => Right(CannedAcl.AwsExecRead)
+      case CannedAcl.BucketOwnerFullControl.value => Right(CannedAcl.BucketOwnerFullControl)
+      case CannedAcl.BucketOwnerRead.value        => Right(CannedAcl.BucketOwnerRead)
+      case CannedAcl.Private.value                => Right(CannedAcl.Private)
+      case CannedAcl.PublicRead.value             => Right(CannedAcl.PublicRead)
+      case CannedAcl.PublicReadWrite.value        => Right(CannedAcl.PublicReadWrite)
+      case rest                                   => Left(failure(cur, rest, "CannedAcl"))
+    }
 
   implicit val metaHeadersConfigReader = mapReader[String].map(MetaHeaders.apply)
 
-  implicit val storageClassConfigReader = new ConfigReader[StorageClass] {
-    override def from(cur: ConfigCursor): Result[StorageClass] =
-      cur.asString.flatMap {
-        case StorageClass.Standard.storageClass          => Right(StorageClass.Standard)
-        case StorageClass.InfrequentAccess.storageClass  => Right(StorageClass.InfrequentAccess)
-        case StorageClass.Glacier.storageClass           => Right(StorageClass.Glacier)
-        case StorageClass.ReducedRedundancy.storageClass => Right(StorageClass.ReducedRedundancy)
-        case rest                                        => Left(failure(cur, rest, "StorageClass"))
-      }
-  }
+  implicit val storageClassConfigReader: ConfigReader[StorageClass] = (cur: ConfigCursor) =>
+    cur.asString.flatMap {
+      case StorageClass.Standard.storageClass          => Right(StorageClass.Standard)
+      case StorageClass.InfrequentAccess.storageClass  => Right(StorageClass.InfrequentAccess)
+      case StorageClass.Glacier.storageClass           => Right(StorageClass.Glacier)
+      case StorageClass.ReducedRedundancy.storageClass => Right(StorageClass.ReducedRedundancy)
+      case rest                                        => Left(failure(cur, rest, "StorageClass"))
+    }
 
-  implicit val serverSideEncryptionReader = new ConfigReader[ServerSideEncryption] {
-    override def from(cur: ConfigCursor): Result[ServerSideEncryption] =
-      cur.fluent.at("type").asString.flatMap {
-        case "aes256" =>
-          Right(ServerSideEncryption.aes256())
-        case "kms" =>
-          ConfigReader
-            .forProduct2("key-id", "context") { (keyId: String, context: Option[String]) =>
-              val base = ServerSideEncryption.kms(keyId)
-              context.fold(base)(base.withContext)
-            }
-            .from(cur)
-        case "customer-keys" =>
-          ConfigReader
-            .forProduct2("key", "md5") { (key: String, md5: Option[String]) =>
-              val base = ServerSideEncryption.customerKeys(key)
-              md5.fold(base)(base.withMd5)
-            }
-            .from(cur)
-      }
-  }
+  implicit val serverSideEncryptionReader: ConfigReader[ServerSideEncryption] = (cur: ConfigCursor) =>
+    cur.fluent.at("type").asString.flatMap {
+      case "aes256" =>
+        Right(ServerSideEncryption.aes256())
+      case "kms" =>
+        ConfigReader
+          .forProduct2("key-id", "context") { (keyId: String, context: Option[String]) =>
+            val base = ServerSideEncryption.kms(keyId)
+            context.fold(base)(base.withContext)
+          }
+          .from(cur)
+      case "customer-keys" =>
+        ConfigReader
+          .forProduct2("key", "md5") { (key: String, md5: Option[String]) =>
+            val base = ServerSideEncryption.customerKeys(key)
+            md5.fold(base)(base.withMd5)
+          }
+          .from(cur)
+    }
 
-  implicit val s3HeadersConfigReader =
+  implicit val s3HeadersConfigReader: ConfigReader[S3Headers] =
     ConfigReader.forProduct5("canned-acl",
                              "meta-headers",
                              "storage-class",
