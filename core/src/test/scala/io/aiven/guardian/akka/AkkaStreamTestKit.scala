4b9593f76777060960b61b8a41907a997525f7e1
Matthew de Detrich <matthew.dedetrich@aiven.io>
2022-01-04 19:48:34 +1100

    Fix fencepost issues with stream algorithm plus fix tests

--- core/src/test/scala/io/aiven/guardian/akka/AkkaStreamTestKit.scala
+++ core/src/test/scala/io/aiven/guardian/akka/AkkaStreamTestKit.scala
@@ -6,9 +6,17 @@ import akka.testkit.TestKitBase
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.Suite
 
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
 trait AkkaStreamTestKit extends TestKitBase with BeforeAndAfterAll { this: Suite =>
   implicit val system: ActorSystem
 
   override protected def afterAll(): Unit =
     TestKit.shutdownActorSystem(system)
+
+  /** If its not possible to determine whether a Stream has finished in a test and instead you need to use a manual
+    * wait, make sure you wait at least this period of time for akka-streams to initialize properly.
+    */
+  val AkkaStreamInitializationConstant: FiniteDuration = 500 millis
 }
