d35d43067e9a4985f5ef07b063dc9058024362da
Matthew de Detrich <matthew.dedetrich@aiven.io>
2022-02-22 14:40:32 +0100

    Check if evaluated environment variable is empty string

--- core/src/test/scala/io/aiven/guardian/kafka/TestUtils.scala
+++ core/src/test/scala/io/aiven/guardian/kafka/TestUtils.scala
@@ -83,7 +83,11 @@ object TestUtils {
 
   def checkEnvVarAvailable(env: String): Boolean =
     try
-      sys.env.contains(env)
+      sys.env.get(env) match {
+        case Some(_)                      => true
+        case Some(value) if value.isBlank => false
+        case None                         => false
+      }
     catch {
       case _: NoSuchElementException => false
     }
