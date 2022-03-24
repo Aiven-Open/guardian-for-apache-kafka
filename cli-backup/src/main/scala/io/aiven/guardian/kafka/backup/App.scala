0927a9cbbbd869f41b7b862b99461d45e09a7068
Matthew de Detrich <matthew.dedetrich@aiven.io>
2022-03-24 17:20:43 +0100

    Log errors if a stream terminates unexpectedly

--- cli-backup/src/main/scala/io/aiven/guardian/kafka/backup/App.scala
+++ cli-backup/src/main/scala/io/aiven/guardian/kafka/backup/App.scala
@@ -3,6 +3,9 @@ package io.aiven.guardian.kafka.backup
 import akka.Done
 import akka.actor.ActorSystem
 import akka.kafka.scaladsl.Consumer
+import akka.stream.ActorAttributes
+import akka.stream.Supervision
+import com.typesafe.scalalogging.StrictLogging
 import io.aiven.guardian.kafka.backup.BackupClientInterface
 import io.aiven.guardian.kafka.backup.KafkaClient
 import io.aiven.guardian.kafka.backup.KafkaClientInterface
@@ -10,13 +13,21 @@ import io.aiven.guardian.kafka.backup.KafkaClientInterface
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
-trait App[T <: KafkaClientInterface] {
+trait App[T <: KafkaClientInterface] extends StrictLogging {
   implicit val kafkaClient: T
   implicit val backupClient: BackupClientInterface[KafkaClient]
   implicit val actorSystem: ActorSystem
   implicit val executionContext: ExecutionContext
 
-  def run(): Consumer.Control = backupClient.backup.run()
+  def run(): Consumer.Control = {
+    val decider: Supervision.Decider = { e =>
+      logger.error("Unhandled exception in stream", e)
+      Supervision.Stop
+    }
+
+    backupClient.backup.withAttributes(ActorAttributes.supervisionStrategy(decider)).run()
+  }
+
   def shutdown(control: Consumer.Control): Future[Done] =
     // Ideally we should be using drainAndShutdown however this isn't possible due to
     // https://github.com/aiven/guardian-for-apache-kafka/issues/80
--- cli-restore/src/main/scala/io/aiven/guardian/kafka/restore/S3App.scala
+++ cli-restore/src/main/scala/io/aiven/guardian/kafka/restore/S3App.scala
@@ -1,11 +1,22 @@
 package io.aiven.guardian.kafka.restore
 
+import akka.stream.{ActorAttributes, Attributes, Supervision}
 import akka.stream.alpakka.s3.S3Settings
+import com.typesafe.scalalogging.StrictLogging
 import io.aiven.guardian.kafka.restore.s3.RestoreClient
 import io.aiven.guardian.kafka.s3.{Config => S3Config}
 
-trait S3App extends S3Config with RestoreApp with App {
+trait S3App extends S3Config with RestoreApp with App with StrictLogging {
   lazy val s3Settings: S3Settings = S3Settings()
   implicit lazy val restoreClient: RestoreClient[KafkaProducer] =
-    new RestoreClient[KafkaProducer](Some(s3Settings), maybeKillSwitch)
+    new RestoreClient[KafkaProducer](Some(s3Settings), maybeKillSwitch) {
+      override val maybeAttributes: Some[Attributes] = {
+        val decider: Supervision.Decider = { e =>
+          logger.error("Unhandled exception in stream", e)
+          Supervision.Stop
+        }
+
+        Some(ActorAttributes.supervisionStrategy(decider))
+      }
+    }
 }
