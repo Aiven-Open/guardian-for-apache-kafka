e50552b223f2cdf35e3481d2080b484686e34f0c
Matthew de Detrich <matthew.dedetrich@aiven.io>
2022-03-29 10:45:40 +0200

    Improve logging for CLI

--- cli-restore/src/main/scala/io/aiven/guardian/kafka/restore/S3App.scala
+++ cli-restore/src/main/scala/io/aiven/guardian/kafka/restore/S3App.scala
@@ -4,11 +4,11 @@ import akka.stream.ActorAttributes
 import akka.stream.Attributes
 import akka.stream.Supervision
 import akka.stream.alpakka.s3.S3Settings
-import com.typesafe.scalalogging.StrictLogging
+import com.typesafe.scalalogging.LazyLogging
 import io.aiven.guardian.kafka.restore.s3.RestoreClient
 import io.aiven.guardian.kafka.s3.{Config => S3Config}
 
-trait S3App extends S3Config with RestoreApp with App with StrictLogging {
+trait S3App extends S3Config with RestoreApp with App with LazyLogging {
   lazy val s3Settings: S3Settings = S3Settings()
   implicit lazy val restoreClient: RestoreClient[KafkaProducer] =
     new RestoreClient[KafkaProducer](Some(s3Settings), maybeKillSwitch) {
